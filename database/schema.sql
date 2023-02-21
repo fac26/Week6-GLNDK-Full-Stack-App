640abefa9a023934dfe768a806e1445ffa801915
Niete <nieteis@gmail.com>
2023-02-21 13:47:09 +0000

    The database can finally be seeded

--- database/schema.sql
+++ database/schema.sql
@@ -1,24 +1,29 @@
+PRAGMA foreign_keys = ON; 
 BEGIN;
 
+
+CREATE TABLE IF NOT EXISTS publishers (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS genres (
+  id INTEGER PRIMARY KEY,
+  name TEXT
+);
+
 CREATE TABLE IF NOT EXISTS comics (
-  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  title TEXT NOT NULL,
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
   issue INTEGER,
   comic_desc TEXT, 
   image_path VARCHAR,
-  price DECIMAL(10,2) NOT NULL,
+  price DECIMAL(10,2),
   publisher_id INTEGER REFERENCES publishers(id) ,
   published_month TEXT,
   published_year INTEGER,
   genre TEXT REFERENCES genre (id)
 );
 
-CREATE TABLE IF NOT EXISTS publishers (
-  id INTEGER NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL
-);
 
-CREATE TABLE IF NOT EXISTS genres (
-  id INTEGER NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL
-);
+COMMIT;
