9dd978b55c38138deb5649ec2cea0ca36af0310d
Niete <nieteis@gmail.com>
2023-02-21 14:22:57 +0000

    Changed model

--- database/model.js
+++ database/model.js
@@ -1,12 +1,12 @@
 import db from "./db.js";
 
+//Gets all comics from the database
 const get_all_Comics = db.prepare(
   /*sql*/
   `SELECT
   id,
   title,
   issue,
-  comic_desc, 
   image_path,
   FORMAT ('£%.2f', price) AS PRICE,
   publisher_id,
@@ -17,14 +17,24 @@ FROM
 comics`
 );
 
-export function getComics() {
+export function getAllComics() {
   return get_all_Comics.all();
 }
 
-const get_comics = db.prepare(
-  /*sql*/ `SELECT id,title,issue, image_path, price, publisher_id, published_month, published_year, genre from comics WHERE id = ?`
-);
+//Gets one single commic by ID
+const get_comic_by_ID = db.prepare(/*sql*/ `SELECT 
+  id,
+  title,
+  issue,
+  comic_desc, 
+  image_path, 
+  FORMAT ('£%.2f', price) AS PRICE, 
+  publisher_id, 
+  published_month, 
+  published_year, 
+  genre 
+  FROM comics WHERE id = ?`);
 
-export function getComics(id) {
-  return get_comics.get(id);
+export function getComicByID(id) {
+  return get_comic.get(id);
 }
