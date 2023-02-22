152a732b9b3f85bd583f0534dba785fd05e48332
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-02-22 16:52:52 +0000

    Final effort

--- components/addToCart.js
+++ components/addToCart.js
@@ -1,17 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BuyBtn } from './comicCard';
 
 const AddToCart = ({ comic }) => {
   const [quantity, setQuantity] = useState([]);
   // [1] Add comic to useState here
-  const [comic, setComic] = useState({});
-
+  const [item, setItem] = useState('');
+  let comicItem = {};
   useEffect(() => {
-    let local = JSON.parse(localStorage.setItem('basket', comic))
-      ? JSON.parse(localStorage.setItem('basket')).length
-      : 0;
-    setItemsInBasket(local);
-  }, []);
+    // Check if there is basket in localstorage -  get item
+    // if there is a basket = add to it the new item
+    // if no basket = set an item
+    console.log(item);
+    //localStorage.setItem('basket', JSON.stringify(comic));
+    //   ? JSON.parse(localStorage.setItem('basket')).length
+    //   : 0;
+  }, [item]);
 
   // [2] Add useEffect to trigger on change to comic
 
@@ -20,12 +23,14 @@ const AddToCart = ({ comic }) => {
   };
 
   const handleAddToCart = () => {
-    const item = {
+    comicItem = {
       id: comic.id,
       title: comic.title,
       quantity: 1,
       price: comic.price,
     };
+
+    setItem(comicItem.title);
 
     // [3] setComic to the comic data above
     // comic.addtoCart(item);
