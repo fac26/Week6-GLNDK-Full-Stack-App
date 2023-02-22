15963f9e0d4f9fd050c816d3b1bffc1fca016eb6
LauraK0 <laurakeating@live.co.uk>
2023-02-22 11:15:39 +0000

    comic book page component rendered on page with data

--- components/comicBookPage.js
+++ components/comicBookPage.js
@@ -4,20 +4,26 @@ import { useState } from "react";
 // import styles from "../styles/"
 //import button from "..components/...."
 
-export default function ComicBookPage({comicBook}) {
-const {title, author, image_path, description, genre, id, price}= comicBook
+// export default function ComicBookPage({ comicBookData }) {
+//   console.log("hello", comicBookData);
+//   return <div>hello</div>;
+// }
+
+export default function ComicBookPage({ comicBookData }) {
+  const { title, author, image_path, comic_desc, genre, id, PRICE } =
+    comicBookData;
   return (
     <section className="comic-book-display">
-      <div className="image-container">
-      <Image src={image_path} alt={title} width={300} height={300} />
-      </div>
+      <div className="image-container"></div>
       <div className="comic-book-info">
         <h1>{title}</h1>
-        <h2>{author}</h2>
         <h3>{genre}</h3>
-        <h4>{price}</h4>
-        <p>{description}</p>
+        <h4>{PRICE}</h4>
+        <Image src={image_path} alt={title} width={300} height={300} />
+        <p>{comic_desc}</p>
       </div>
     </section>
   );
 }
+
+//         <Image src={image_path} alt={title} width={300} height={300} />
