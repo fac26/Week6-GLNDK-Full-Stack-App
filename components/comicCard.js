95d62a3d7f42900edabada470bd850a5405415e3
LauraK0 <laurakeating@live.co.uk>
2023-02-23 09:46:24 +0000

    styles correct now

--- components/comicCard.js
+++ components/comicCard.js
@@ -22,19 +22,23 @@ export default function ComicCard({ comicsData, publisher }) {
     return (
       <>
         {comicsData.map((comic) => (
-          <div className="comic-container" key={comic.id}>
+          <div className={style.comicContainer} key={comic.id}>
             <Link href={`/comics/${comic.id}`}>
-              <Image src={comic.image_path} width={300} height={500} />
+              <Image src={comic.image_path} width={300} height={425} />
             </Link>
-            <p className="title">{comic.title}</p>
-            <p className="issue">{`#${comic.issue}`}</p>
+
+            <p className={style.title}>
+              {comic.title} {`#${comic.issue}`}
+            </p>
+
             <p className="publisher">{comic.publisher_name}</p>
             <p className="published">
               {`${comic.published_month} ${comic.published_year}`}
             </p>
             <p className="genre">{comic.genre_name}</p>
-            <p className="price">{comic.price}</p>
-            <button className="buyBtn">Add to Cart</button>
+
+            <p className={style.price}>{comic.PRICE}</p>
+            <button className={style.button}>Add to Cart</button>
           </div>
         ))}
       </>
@@ -50,26 +54,25 @@ export default function ComicCard({ comicsData, publisher }) {
             publisher.filter === 'all'
         )
         .map((comic) => (
-        <div className={style.comicContainer} key={comic.id}>
-          <Link href={`/comics/${comic.id}`}>
-            <Image src={comic.image_path} width={300} height={425} />
-          </Link>
+          <div className={style.comicContainer} key={comic.id}>
+            <Link href={`/comics/${comic.id}`}>
+              <Image src={comic.image_path} width={300} height={425} />
+            </Link>
 
-          <p className={style.title}>
-            {comic.title} {`#${comic.issue}`}
-          </p>
+            <p className={style.title}>
+              {comic.title} {`#${comic.issue}`}
+            </p>
 
-          <p className="publisher">{comic.publisher_name}</p>
-          <p className="published">
-            {`${comic.published_month} ${comic.published_year}`}
-          </p>
-          <p className="genre">{comic.genre_name}</p>
+            <p className="publisher">{comic.publisher_name}</p>
+            <p className="published">
+              {`${comic.published_month} ${comic.published_year}`}
+            </p>
+            <p className="genre">{comic.genre_name}</p>
 
-          <p className={style.price}>{comic.PRICE}</p>
-          <button className={style.button}>Add to Cart</button>
-
-        </div>
-      ))}
+            <p className={style.price}>{comic.PRICE}</p>
+            <button className={style.button}>Add to Cart</button>
+          </div>
+        ))}
     </>
   );
 }
